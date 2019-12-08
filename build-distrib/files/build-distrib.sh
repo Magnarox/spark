180665fed3b0d27bf261cd791ec7472ef307c3d6
Magnarox <magnarox06@gmail.com>
2019-12-08 21:11:47 +0100

    Update build-distrib.sh

--- build-distrib/files/build-distrib.sh
+++ build-distrib/files/build-distrib.sh
@@ -11,3 +11,10 @@ export MAVEN_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m"
 ./dev/make-distribution.sh --name magnarox-spark --pip --r --tgz -Psparkr -Phadoop-3.1 -Dhadoop.version=3.1.2 -Phive -Phive-thriftserver -Pkubernetes
 
 cp *.tgz ../out/
+
+cd ../out/
+tar xvf spark-2.4.4-bin-magnarox-spark.tgz
+cp -R ../spark-2.4.4/licenses/ spark-2.4.4-bin-magnarox-spark/
+cp custom-jars/*.jar spark-2.4.4-bin-magnarox-spark/jars/
+rm spark-2.4.4-bin-magnarox-spark.tgz
+tar czf spark-2.4.4-bin-magnarox-spark.tgz spark-2.4.4-bin-magnarox-spark
